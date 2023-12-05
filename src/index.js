acb651259efdf269f34fe380837bb20ca9a1fcd9
ImsuperSir <imsupersir@gmail.com>
2023-12-05 02:39:01 -0800

    gettingurl

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ const apiKey = 'tEvmShhCKx3ldivNkeJVpr75ZyPjpOO6';
 
 const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
 
-peticion.then( (resp) => {
-    resp.json().then( data => {
-        console.log(data)
-    });
-    //console.log(resp);
-})
-.catch( (err) => {
-    console.warn(err);
-})
+peticion
+    .then(resp => resp.json())  //esto es una respuesta implicita de la promesa, que es una promesa, y se puede encadenar
+    .then( ({data}) => {
+        //console.log(data.images.original.url);
+        const { url } = data.images.original; //desestructuracion
+        // console.log(data.images.original)
+        // console.log(url);
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+
+    })   
+    .catch((err) =>
+    {
+        console.warn(err);
+    })
